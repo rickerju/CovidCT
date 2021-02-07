9eb11fa2093e9b16610df94bf1fef57e02f79323
Justin Rickert <rickerju@mail.gvsu.edu>
2021-02-06 22:10:20 -0500

    small zip

---
+++ split_data.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+dirname = os.path.dirname(__file__)
+data_directory = os.path.join(dirname, 'data')
+
+data_size = 1000
+
+i = 0
+current_directory = ''
+
+
+os.mkdir('zip')
+for f in os.listdir(data_directory):
+    if i % data_size is 0:
+        current_directory = 'data-' + str(i)
+        os.mkdir('zip/' + current_directory)
+
+    shutil.copy('data/' + f, 'zip/' + current_directory)
+
+    i = i + 1
